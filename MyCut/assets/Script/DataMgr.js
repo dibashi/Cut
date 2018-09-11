85e1b18e1dfded7c51dfb77f83c3fbd3ffd230c7
dibashi <dibashi1989@163.com>
2018-09-11 10:26:27 +0800

    第四关 铰链bug

--- MyCut/assets/Script/DataMgr.js
+++ MyCut/assets/Script/DataMgr.js
@@ -41,7 +41,7 @@ export default class DataMgr extends cc.Component {
             var checkPointJsonData = [];
             var j = {};
             checkPointJsonData.push({crownCount:"0"});
-            for (var i = 1; i < 3; i++) {
+            for (var i = 1; i < 4; i++) {
                 j.crownCount = "0";
                 
                 checkPointJsonData.push(j);
